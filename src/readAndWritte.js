51fad0588ffc1de33588f0e1129183cab3662d1b
Leticia-C <leticia.camposg93@gmail.com>
2022-08-30 13:16:00 -0300

    Corrige 3

--- src/readAndWritte.js
+++ src/readAndWritte.js
@@ -13,7 +13,6 @@ const getATalkerById = async (id) => {
 
 const generateToken = () => crypto.randomBytes(8).toString('hex');
 
-module.exports = generateToken;
 module.exports = {
     readTalker,
     getATalkerById,
--- src/router/loginRouter.js
+++ src/router/loginRouter.js
@@ -1,12 +1,13 @@
 const express = require('express');
-const { generateToken } = require('../readAndWritte');
+// const { generateToken } = require('../readAndWritte');
+const crypto = require('crypto');
 const { emailValidation } = require('../middleware/emailValidation');
 const { passwordValidation } = require('../middleware/passwordValidation');
 
 const loginRouter = express.Router();
 
-loginRouter.post('/', emailValidation, passwordValidation, async (req, res) => {
-  const result = await generateToken();
+loginRouter.post('/', emailValidation, passwordValidation, (req, res) => {
+   const result = crypto.randomBytes(8).toString('hex');
     res.status(200).json({ token: result });
   });
 
