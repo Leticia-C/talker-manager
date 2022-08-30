cfd3a4eb8d02e7796da8f2a9dfce63024f6e3084
Leticia-C <leticia.camposg93@gmail.com>
2022-08-30 12:38:10 -0300

    Adiciona requisito 4, mas quebra o 3

--- src/router/loginRouter.js
+++ src/router/loginRouter.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const { generateToken } = require('../readAndWritte');
+const { emailValidation } = require('../middleware/emailValidation');
+const { passwordValidation } = require('../middleware/passwordValidation');
 
 const loginRouter = express.Router();
 
-loginRouter.post('/', async (req, res) => {
-  const result = generateToken();
+loginRouter.post('/', emailValidation, passwordValidation, async (req, res) => {
+  const result = await generateToken();
     res.status(200).json({ token: result });
   });
 
