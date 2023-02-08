ff04e4c4c17a2f39bfc88b07d80a7b7763bda542
Leticia-C <leticia.camposg93@gmail.com>
2023-02-08 20:49:30 -0300

    Adiciona modiicação no index e no loginRouter

--- src/router/loginRouter.js
+++ src/router/loginRouter.js
@@ -5,7 +5,7 @@ const { passwordValidation } = require('../middleware/passwordValidation');
 
 const loginRouter = express.Router();
 
-loginRouter.post('/', emailValidation, passwordValidation, async (req, res) => {
+loginRouter.post('/', emailValidation, passwordValidation, async (_req, res) => {
      const result = generateToken();
     res.status(200).json({ token: result });
   });
